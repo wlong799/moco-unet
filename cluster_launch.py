a36796ae25001b2b7877856fae2564063f8239e2
Will Long <wlong799@gmail.com>
2021-09-17 09:55:31 -0400

    Remove port search from cluster script

--- cluster_launch.py
+++ cluster_launch.py
@@ -1,5 +1,4 @@
 import os
-import socket
 
 ROOT_DIR = '/groups/stringer/stringerlab/PoseEstimation/moco-unet/'
 # ROOT_DIR = '/home/labadmin/Documents/moco-unet'
@@ -8,19 +7,18 @@ CODE_DIR = 'code'
 DATA_DIR = 'data'
 
 NUM_GPUS = 4
-WALLTIME = 600
+WALLTIME = 300
 
 START_PORT = 10001
-CONNECT_ATTEMPTS = 100
 
 
 def main():
     os.chdir(ROOT_DIR)
     opt_args = {
-        'learning-rate': [0.001, 0.003, 0.01, 0.03, 0.1, 0.3],
-        # 'moco-k': [128, 256, 512, 1024],
-        # 'mlp': [False, True],
-        # 'cos': [False, True]
+        'learning-rate': [0.003],
+        'moco-k': [256, 512, 1024],
+        'mlp': [False, True],
+        'cos': [False, True]
     }
     datasets = ['unsupervised_8k']
 
@@ -54,11 +52,6 @@ def main():
     for dataset in datasets:
         data_dir = os.path.join(DATA_DIR, dataset)
         for command, save_name in zip(python_commands, save_names):
-            attempts = 0
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                while s.connect_ex(('localhost', port)) == 0 and attempts < CONNECT_ATTEMPTS:
-                    port += 1
-                    attempts += 1
             save_dir = os.path.join(CHECKPOINTS_DIR, save_name + f'_{dataset}')
             os.makedirs(save_dir, exist_ok=True)
             output_file = os.path.join(save_dir, 'output.txt')
@@ -70,7 +63,7 @@ def main():
 
     for command in bsub_commands:
         os.system(command)
-        print(command)
+        # print(command)
 
 
 if __name__ == '__main__':
