2612b21cda68f4ea09ad0b62f622008b32823519
Will Long <wlong799@gmail.com>
2021-09-17 11:25:31 -0400

    Create script to train UNets on cluster

---
+++ cluster_launch_unet.py
@@ -0,0 +1,43 @@
+import os
+from glob import glob
+
+ROOT_DIR = '/groups/stringer/stringerlab/PoseEstimation/moco-unet/'
+CHECKPOINT_DIR = 'checkpoints'
+DATA_DIR = 'data/supervised'
+AUG_FILE = 'aug_params.yml'
+
+MOCO_PREFIX = 'moco_unet'
+SAVE_PREFIX = 'unet_pretrained'
+CHECKPOINT_NAME = 'checkpoint_0200.pth.tar'
+
+
+def main():
+    os.chdir(ROOT_DIR)
+
+    base_bsub_command = f'bsub -n 4 -gpu "num=1" -q gpu_rtx -W 600'
+    base_python_command = f'python code/main_unet.py -b 32'
+
+    model_paths = glob(os.path.join(CHECKPOINT_DIR, f'{MOCO_PREFIX}*'))
+    for model_path in model_paths:
+        checkpoint = os.path.join(model_path, CHECKPOINT_NAME)
+        m_ids = model_path.split('_')
+        start = m_ids.index('0.003')
+        end = m_ids.index('cos')
+        save_id = m_ids[start + 1:end]
+        save_name = f'{SAVE_PREFIX}_{"_".join(save_id)}'
+        save_dir = os.path.join(CHECKPOINT_DIR, save_name)
+        output_file = os.path.join(save_dir, 'output.txt')
+        error_file = os.path.join(save_dir, 'error.txt')
+
+        python_command = f'{base_python_command} --pretrained {checkpoint} ' \
+                         + f'{DATA_DIR} {save_dir} {AUG_FILE}'
+        bsub_command = f'{base_bsub_command} -J {save_name} -o {output_file} ' \
+                       + f'-e {error_file} "{python_command}"'
+
+        os.makedirs(save_dir, exist_ok=True)
+        os.system(bsub_command)
+        # print(bsub_command, end='\n\n')
+
+
+if __name__ == '__main__':
+    main()
